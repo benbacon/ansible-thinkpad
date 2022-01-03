b041b0399a7eff5fff290b42364feb2dec4d7721
ben <github@baco.xyz>
2022-01-03 00:29:26 +0000

    Various bugfixes found during testing

--- main.sh
+++ main.sh
@@ -5,4 +5,5 @@ python -m venv ~/venv/ansible-thinkpad
 source ~/venv/ansible-thinkpad/bin/activate
 python -m pip install -r pip-requirements.txt
 ansible-galaxy install -r requirements.yml
-ansible-playbook main.yml --ask-become-pass
+sudo echo "privilege escalation" > /dev/null
+ansible-playbook main.yml
